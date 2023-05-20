dce321ce0d24fe17068733bf602d9db7568700dd
orangepulpsucks <42681894+orangepulpsucks@users.noreply.github.com>
2023-05-20 00:44:18 -0700

    Initial Framework Setup

---
+++ app.py
@@ -0,0 +1,13 @@
+import psycopg2
+from flask import Flask
+from psycopg2.extras import RealDictCursor
+
+conn = psycopg2.connect(
+    "host=movie dbname=movie user=movie password=movie",
+    cursor_factory=RealDictCursor)
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
