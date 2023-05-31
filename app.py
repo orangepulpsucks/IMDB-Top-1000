53e8b3b62862dcf9513b18b9cf4e417563ca1e2b
orangepulpsucks <hahya@plu.edu>
2023-05-31 23:07:15 +0000

    Container Update

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import csv
 from flask import Flask, request, render_template
-import psycopg2
+import psycopg2, random
+
 from psycopg2.extras import RealDictCursor
 
 conn = psycopg2.connect(
-    "host=movies dbname=movie user=movie password=movie",
+    "host=db dbname=postgres user=postgres password=postgres",
     cursor_factory=RealDictCursor)
 app = Flask(__name__)
+
 
 
 @app.route('/')
