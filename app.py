08f5954e2f8744b54fde18416ea946dd2088e6cb
orangepulpsucks <hahnya@plu.edu>
2023-06-09 23:17:04 +0000

    Update home.html

--- app.py
+++ app.py
@@ -290,7 +290,7 @@ def render_sets3():
         results3 = list(cur.fetchall())  
 
 
-    return render_template("homeß.html",
+    return render_template("home.html",
                            movie=results3, 
                            params3=request.args
                            )
