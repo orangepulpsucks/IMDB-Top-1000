4e85228673bb55953df12f206606804bd817a3e8
orangepulpsucks <hahnya@plu.edu>
2023-06-13 06:57:42 +0000

    Done for tonight!

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import csv
 from flask import Flask, request, render_template
-import psycopg2, random
+import psycopg2, random, datetime
 
 from psycopg2.extras import RealDictCursor
 
@@ -8,6 +8,10 @@ conn = psycopg2.connect(
     "host=db dbname=postgres user=postgres password=postgres",
     cursor_factory=RealDictCursor)
 app = Flask(__name__)
+
+# Random Series Cache
+random_series_cache = None
+cache_date = None
 
 # TEMPLATE
 # @app.route("/about")
@@ -247,7 +251,7 @@ def render_sets2():
         "sort_by": sort_by,
         "sort_dir" : sort_dir,
         "limit" : limit
-}
+    }
 
     def get_sort_dir2(col):
         if col== sort_by:
@@ -308,12 +312,13 @@ def render_sets2():
                            result_old_boy_overview=old_boy_overview
                            )
 
+
 @app.route("/")
-def render_sets3():
+def render_sets6():
     poster_link = request.args.get("poster_link", "")
     series_title = request.args.get("series_title", "")
     released_year = request.args.get("released_year", "")
-    certificate = request.args.get("certificate", "") 
+    certificate = request.args.get("certificate", "")
     runtime = request.args.get("runtime", "")
     genre = request.args.get("genre", "")
     imdb_rating = request.args.get('imdb_rating')
@@ -331,7 +336,7 @@ def render_sets3():
     sort_dir = request.args.get("sort_dir", "asc")
     limit = request.args.get("limit", 1000, type=int)
 
-    from_where_clause3 = """
+    from_where_clause6 = """
         from movie
         where %(series_title)s is null or series_title ilike %(series_title)s
         and ( %(released_year)s is null or released_year = %(released_year)s )
@@ -341,7 +346,7 @@ def render_sets3():
         and ( %(director)s is null or director ilike %(director)s )
     """
 
-    params3 = {
+    params6 = {
         "poster_link": f"%{poster_link}%",
         "series_title": f"%{series_title}%",
         "released_year": int(released_year) if released_year and released_year.isdigit() else 0
@@ -366,29 +371,105 @@ def render_sets3():
         "sort_by": sort_by,
         "sort_dir" : sort_dir,
         "limit" : limit
-}
+    }
 
-    def get_sort_dir3(col):
-        if col== sort_by:
+    def get_sort_dir6(col):
+        if col == sort_by:
             return "desc" if sort_dir == "asc" else "asc"
         else:
             return "asc"
-        
+
     with conn.cursor() as cur:
         cur.execute(f"""select series_title, released_year, runtime, genre, imdb_rating, director, poster_link, certificate, overview, meta_score, star1, star2, star3, star4, no_of_votes, gross 
-                        {from_where_clause3} 
+                        {from_where_clause6} 
                         order by {sort_by} {sort_dir} 
                         limit %(limit)s 
                     """,
-                    params3)
-        results3 = list(cur.fetchall())  
-       
-        # Select a random movie name from the results
-        random_series = random.choice(results3)
+                    params6)
+        results6 = list(cur.fetchall())
+
+    def get_random_series():
+        global random_series_cache, cache_date
+
+        # Check if the cache is empty or it's a new day
+        if random_series_cache is None or cache_date != datetime.date.today():
+            with conn.cursor() as cur:
+                cur.execute("SELECT COUNT(*) FROM movie")
+                count = cur.fetchone()["count"]
+
+                # Select a random offset
+                random_offset = random.randint(0, count - 1)
+
+                # Get the random series
+                cur.execute("SELECT series_title FROM movie OFFSET %s LIMIT 1", (random_offset,))
+                random_series_cache = cur.fetchone()["series_title"]
+
+            cache_date = datetime.date.today()
+
+        return random_series_cache
+
+    # Get the random series
+    random_series = get_random_series()
+
+    # Manually setting what I want to display for MOTD
+    with conn.cursor() as cur:
+
+        # Director
+        cur.execute("SELECT director AS motd_director FROM movie WHERE series_title ILIKE %s", (f"%{random_series}%",))
+        motd_director = cur.fetchone()["motd_director"]
+
+        # Year
+        cur.execute("SELECT released_year AS motd_year FROM movie WHERE series_title ILIKE %s", (f"%{random_series}%",))
+        motd_year = cur.fetchone()["motd_year"]
+
+        # Poster
+        cur.execute("SELECT poster_link AS motd_poster FROM movie WHERE series_title ILIKE %s", (f"%{random_series}%",))
+        motd_poster = cur.fetchone()["motd_poster"]
+
+        # Star1
+        cur.execute("SELECT star1 AS motd_star1 FROM movie WHERE series_title ILIKE %s", (f"%{random_series}%",))
+        motd_star1 = cur.fetchone()["motd_star1"]
+
+        # Star2
+        cur.execute("SELECT star2 AS motd_star2 FROM movie WHERE series_title ILIKE %s", (f"%{random_series}%",))
+        motd_star2 = cur.fetchone()["motd_star2"]
+
+        # Star3
+        cur.execute("SELECT star3 AS motd_star3 FROM movie WHERE series_title ILIKE %s", (f"%{random_series}%",))
+        motd_star3 = cur.fetchone()["motd_star3"]
+
+        # Star4
+        cur.execute("SELECT star4 AS motd_star4 FROM movie WHERE series_title ILIKE %s", (f"%{random_series}%",))
+        motd_star4 = cur.fetchone()["motd_star4"]
+
+        # Overview
+        cur.execute("SELECT overview AS motd_overview FROM movie WHERE series_title ILIKE %s", (f"%{random_series}%",))
+        motd_overview = cur.fetchone()["motd_overview"]
+
+        # Rating
+        cur.execute("SELECT imdb_rating AS motd_rating FROM movie WHERE series_title ILIKE %s", (f"%{random_series}%",))
+        motd_rating = cur.fetchone()["motd_rating"]
+
+        # Title
+        cur.execute("SELECT series_title AS motd_title FROM movie WHERE series_title ILIKE %s", (f"%{random_series}%",))
+        motd_title = cur.fetchone()["motd_title"]
 
 
     return render_template("home.html",
-                           result_random_series=random_series, 
-                           params3=request.args
+                           result_motd_title=motd_title,
+                           result_random_series=random_series,
+                           result_motd_director=motd_director,
+                           result_motd_year=motd_year,
+                           result_motd_poster=motd_poster,
+                           result_motd_star1=motd_star1,
+                           result_motd_star2=motd_star2,
+                           result_motd_star3=motd_star3,
+                           result_motd_star4=motd_star4,
+                           result_motd_overview=motd_overview,
+                           result_motd_rating=motd_rating
                            )
- 
+
+
+if __name__ == "__main__":
+    app.run()
+
